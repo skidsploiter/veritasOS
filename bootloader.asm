ddd00b9073e97562f78db9f32941844e60043b61
kayzee <190188179+skidsploiter@users.noreply.github.com>
2025-07-07 22:42:14 +0530

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -1,112 +1,85 @@
-bits 16                                ; Let NASM know to use 16 bit mode
-org 0x7C00                             ; Bootloader memory offset
-boot: jmp main                         ; Jump over non-executable header info
-nop                                    ; Moves the BPB to 0x0703
+bits 16
+org 0x7C00
 
-bsOemName:            DB "vOSBeta1"    ; OEM identifier (8 BYTES)
-bpbBytesPerSector:    DW 0x0200        ; 512 Bytes per logical sector (WORD)
-bpbSectorsPerCluster: DB 0x01          ; 1 Logical sector per cluster (BYTE)
-bpbReservedSectors:   DW 0x0001        ; 1 Reserved logical sector (WORD)
-bpbNumberOfFATs:      DB 0x02          ; 2 FATs on the storage media (BYTE)
-bpbRootEntries:       DW 0x00E0        ; 224 Root directory entries (WORD)
-bpbTotalSectors:      DW 0x0B40        ; 2880 Total logical sectors in volume
-                                       ; (WORD)
-bpbMedia:             DB 0xF0          ; Media Descriptor (BYTE): F0 = 1.44kB
-                                       ; 3.5-inch disk
-bpbSectorsPerFAT:     DW 0x0009        ; 9 Logical sectors per FAT (WORD)
-bpbSectorsPerTrack:   DW 0x0012        ; 18 Physical sectors per track (WORD)
-bpbHeadsPerCylinder:  DW 0x0002        ; 2 Heads (WORD)
-bpbHiddenSectors:     DD 0x00000000    ; Hidden sectors (DWORD)
-bpbTotalSectorsBig:   DD 0x00000000    ; Large total logical sectors (DWORD)
-bsDriveNumber:        DB 0x00          ; Physical drive number (BYTE)
-bsUnused:             DB 0x00          ; ExtFlags (BYTE)
-bsExtBootSignature:   DB 0x29          ; Extended boot signature (BYTE). 0x29
-                                       ; indicates that the EBPB has the
-                                       ; following 3 entries:
-bsSerialNumber:       DD 0x00010203    ; Volume serial number (DWORD)
-bsVolumeLabel:        DB "V_OS__10"    ; Volume label (11 BYTES)
-bsFileSystem:         DB "FAT12   "    ; File-system type (8 BYTES)
+boot: jmp main
+nop
 
+; BIOS crap
+bsOemName:            db "vOSBeta1"
+bpbBytesPerSector:    dw 0x0200
+bpbSectorsPerCluster: db 0x01
+bpbReservedSectors:   dw 0x0001
+bpbNumberOfFATs:      db 0x02
+bpbRootEntries:       dw 0x00E0
+bpbTotalSectors:      dw 0x0B40
+bpbMedia:             db 0xF0
+bpbSectorsPerFAT:     dw 0x0009
+bpbSectorsPerTrack:   dw 0x0012
+bpbHeadsPerCylinder:  dw 0x0002
+bpbHiddenSectors:     dd 0x00000000
+bpbTotalSectorsBig:   dd 0x00000000
+bsDriveNumber:        db 0x00
+bsUnused:             db 0x00
+bsExtBootSignature:   db 0x29
+bsSerialNumber:       dd 0x00010203
+bsVolumeLabel:        db "V_OS__10"
+bsFileSystem:         db "FAT12   "
+
+; some messages to show it’s alive
 bootloader_started_msg db "Boot loader started...", 0x0D, 0x0A, 0
-floppy_reset_msg db "Floppy disk reset...", 0x0D, 0x0A, 0
-kernel_loaded_msg db "Kernel loaded...", 0x0D, 0x0A, 0
-
-; The following message should never appear
+floppy_reset_msg       db "Floppy disk reset...", 0x0D, 0x0A, 0
+kernel_loaded_msg      db "Kernel loaded...", 0x0D, 0x0A, 0
 bootloader_complete_msg db "Bootloader complete.", 0x0D, 0x0A, 0
 
-; ******************************************************************************
-; Functions
-; ******************************************************************************
-
-; Print message located at the address stored in the SI register
+; prints stuff
 print_msg:
-    mov ah,0x0E                        ; Teletype function code for INT 10
-.start_loop:                           ; Loop start point label
-    lodsb                              ; Load the char at the SI address
-                                       ; into AL and go to the next char
-    cmp al,0x00                        ; Compare AL to 0
-    je .end_loop                       ; Jump to end_loop if AL equals 0
-    int 0x10                           ; Call INT 10 to print the char
-                                       ; in AL to the screen
-    jmp .start_loop                    ; Jump back to the loop start point
-.end_loop:                             ; End of the loop label
-    ret                                ; return control to the caller
+    mov ah, 0x0E
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    int 0x10
+    jmp .loop
+.done:
+    ret
 
-; Reset the floppy controller to the first sector on the disk
+; floppy go brr
 reset_floppy:
-    mov ah,0x00                        ; Reset Disk Drives function code for
-                                       ; INT 13
-    mov dl,0x00                        ; Set 1st floppy drive as drive to reset
-    int 0x13                           ; Call INT 13 to reset the drive
-    jc reset_floppy                    ; Try again if carry flag (CF) is set
-                                       ; (indicates an error)
-    mov si, floppy_reset_msg           ; Move the address of msg into the
-                                       ; SI register
-    call print_msg                     ; call the output subroutine
-    ret                                ; return control to the caller
+    mov ah, 0x00
+    mov dl, 0x00
+    int 0x13
+    jc reset_floppy
+    mov si, floppy_reset_msg
+    call print_msg
+    ret
 
-; Load the kernel 
+; load kernel from floppy because why not
 read_kernel:
-    mov ax,0x1000
-    mov es,ax                          ; Set the Extra Segment (ES) value to
-                                       ; 4096
-    xor bx,bx                          ; Set the BX register to 0
-                                       ; The kernel's address will be 1000:0000
-    mov ah,0x02                        ; Read Sectors From Drive function code
-                                       ; for INT 13
-    mov al,0x01                        ; Sectors to read count
-    mov ch,0x00                        ; Cylinder
-    mov cl,0x02                        ; Sector to read
-    mov dh,0x00                        ; Head number
-    mov dl,0x00                        ; Drive number
-    int 0x13                           ; BIOS Interrupt Call
-    jc read_kernel                     ; Try again if carry flag (CF) is set
-                                       ; (indicates an error)
-    mov si, kernel_loaded_msg          ; Move the address of msg into the
-                                       ; SI register
-    call print_msg                     ; call the output subroutine 
-    jmp 0x1000:0x0000                  ; Jump to kernel
+    mov ax, 0x1000
+    mov es, ax
+    xor bx, bx
+    mov ah, 0x02
+    mov al, 0x01
+    mov ch, 0x00
+    mov cl, 0x02
+    mov dh, 0x00
+    mov dl, 0x00
+    int 0x13
+    jc read_kernel
+    mov si, kernel_loaded_msg
+    call print_msg
+    jmp 0x1000:0x0000
 
-; ******************************************************************************
-; Main function
-; ******************************************************************************
-
+; main stuff happens here (barely)
 main:
-    mov si, bootloader_started_msg     ; Move the address of msg into the
-                                       ; SI register
-    call print_msg                     ; call the output subroutine
-    call reset_floppy                  ; call the reset subroutine 
-    call read_kernel                   ; call the load kernel subroutine 
+    mov si, bootloader_started_msg
+    call print_msg
+    call reset_floppy
+    call read_kernel
+    ; if you see this, something’s probably broken
+    mov si, bootloader_complete_msg
+    call print_msg
 
-    ; Bootloader is complete at this time and this message should never appear
-    mov si, bootloader_complete_msg    ; Move the address of msg into the
-                                       ; SI register
-    call print_msg                     ; call the output subroutine
-
-; ******************************************************************************
-; Boot signature
-; ******************************************************************************
-
-times 510 - ($-$$) db 0                ; Pad the rest of the bootloader with
-                                       ; zeros
-dw 0xAA55                              ; Add the Boot signature
+; boot sig or BIOS throws a fit
+times 510 - ($ - $$) db 0
+dw 0xAA55
