3477ed62e7487d6f2752224f7a68ab8aaf826c2e
kayzee <190188179+skidsploiter@users.noreply.github.com>
2025-07-07 23:02:19 +0530

    Update comp.bat

--- comp.bat
+++ comp.bat
@@ -5,53 +5,74 @@ setlocal enabledelayedexpansion
 :: proceed only if you have at least 2 brain cells and a dream
 
 :: ==== Tools ====
-set NASM=nasm              :: the almighty assembler
-set QEMU=qemu-system-i386  :: qemu i386 emulator
+set NASM=nasm              
+:: the almighty assembler, turning ASM into ones and zeros
+set QEMU=qemu-system-i386  
+:: retro time machine, boot your stuff like it's 1995
 
 :: ==== File names ====
-set BOOTLOADER=bootloader.asm  :: BIOS says hi
-set KERNEL=kernel.asm          :: the meat of the sandwich
-set BOOTBIN=bootloader.bin     :: bootloader but it's binary now
-set KERNELBIN=kernel.bin       :: kernel.exe but ancient
-set IMAGE=floppy.img           :: because we like pain and nostalgia
-set PADDING=pad.bin            :: pure zeroes, like my attention span
+set BOOTLOADER=bootloader.asm  
+:: BIOS says hi
+set KERNEL=kernel.asm          
+:: the meat of the sandwich
+set BOOTBIN=bootloader.bin     
+:: bootloader but it's binary now
+set KERNELBIN=kernel.bin       
+:: kernel.exe but ancient
+set IMAGE=floppy.img           
+:: because we like pain and nostalgia
+set PADDING=pad.bin            
+:: pure zeroes, like my attention span
 
 :: ==== Image size ====
-set FLOPPY_SIZE=1474560        :: 1.44MB of raw 90s power
+set FLOPPY_SIZE=1474560        
+:: 1.44MB of raw 90s power
 :: make it 1.44MB cuz it aint 1994 without them
 :: basically just vibes and tiny storage
 
 :: ==== Cleanup ====
 echo [*] Cleaning old files...
 :: basically remove the shit i spilled
-del %BOOTBIN% 2>nul            :: bye bootloader
-del %KERNELBIN% 2>nul          :: bye kernel
-del %IMAGE% 2>nul              :: bye floppy
-del %PADDING% 2>nul            :: bye zeroes
+del %BOOTBIN% 2>nul            
+:: bye bootloader
+del %KERNELBIN% 2>nul          
+:: bye kernel
+del %IMAGE% 2>nul              
+:: bye floppy
+del %PADDING% 2>nul            
+:: bye zeroes
 
 :: ==== Assemble bootloader ====
 echo [*] Assembling bootloader...
 %NASM% -f bin %BOOTLOADER% -o %BOOTBIN%
-if errorlevel 1 goto error     :: if NASM cries, we die
+if errorlevel 1 goto error     
+:: if NASM cries, we die
 
 :: ==== Assemble kernel ====
 echo [*] Assembling kernel...
 %NASM% -f bin %KERNEL% -o %KERNELBIN%
-if errorlevel 1 goto error     :: same deal, don't mess it up
+if errorlevel 1 goto error     
+:: same deal, don't mess it up
 
 :: ==== Create floppy image ====
 echo [*] Creating floppy image...
-copy /b %BOOTBIN%+%KERNELBIN% %IMAGE% >nul  		:: i have no idea what this does but ok
+copy /b %BOOTBIN%+%KERNELBIN% %IMAGE% >nul  		
+:: i have no idea what this does but ok
 
 :: Pad image to 1.44MB
-fsutil file createnew %PADDING% %FLOPPY_SIZE% :: generate pure zeros from the void
-copy /b %IMAGE%+%PADDING% padded.img >nul     :: fatten the floppy
-move /y padded.img %IMAGE% >nul              :: overwrite original like a savage
-del %PADDING%                                :: burn the evidence
+fsutil file createnew %PADDING% %FLOPPY_SIZE% 
+:: generate
+copy /b %IMAGE%+%PADDING% padded.img >nul     
+:: fatten the floppy
+move /y padded.img %IMAGE% >nul              
+:: overwrite original
+del %PADDING%                                
+:: burn the evidence
 
 :: ==== Boot with QEMU ====
 echo [*] Booting in QEMU...
-%QEMU% -fda %IMAGE% -boot a -m 64             :: slap it into QEMU, pray for BIOS
+%QEMU% -fda %IMAGE% -boot a -m 64             
+:: slap it into QEMU, pray for BIOS
 
 goto end
 
